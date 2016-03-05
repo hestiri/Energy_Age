dc35bd9d745c317d9afcf05437611d01435caa83
Ábtin Estiri <estiri.hossein@gmail.com>
2016-03-04 23:43:48 -0800

    Analysis script that includes setting up the formulas and regression function

--- Analysis.R
+++ Analysis.R
@@ -65,9 +65,7 @@ detach(d09)
 OUTPUT <- rbind(out1,out2,out3,out4,out5,out6,out7,out8,out9,out10,out11,out12)
 rm(out1,out2,out3,out4,out5,out6,out7,out8,out9,out10,out11,out12)
 
-##LEt's plot'em out!
-ggplot()+geom_point(data=OUTPUT, aes(x=Var, y=Coef, fill = MDL))+
-  facet_wrap(~ YEAR)
+
 
 
 
