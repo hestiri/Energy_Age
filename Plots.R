db00422bef49a8e9fb04ce384cc02648e5c2b8e5
Ábtin Estiri <estiri.hossein@gmail.com>
2016-05-07 17:58:41 -0700

    plotting script.

--- Plots.R
+++ Plots.R
@@ -9,6 +9,26 @@ Ages <- subset(OUTPUT, OUTPUT$Var %in% c("age05","age05to09","age10to14","age15t
                                          "age60to64","age65to69","age70to74","age75to79","age80p"))
 
 
+
+########
+# SMOOTHING *****
+###############
+
+Ages$Coef <- as.integer(Ages$Coef)
+
+
+# creating a variable for x axis to run smoothing regression
+
+Ages$REGX <- 0
+r <- length(unique(Ages$Var))
+for (i in 1:r) {
+  var <- unique(Ages$Var)[i]
+  num <- rank(unique(Ages$Var))[i]
+  Ages$REGX <- ifelse(Ages$Var == var, rank(unique(Ages$Var))[i], Ages$REGX)
+}
+
+
+
 ##########
 ######
 ######## AGE PLOTS
@@ -27,21 +47,41 @@ ggplot(Ages, aes(x = Var, y = Coef)) + theme_bw() + geom_bar(stat = "identity") 
 
 
 
-ggplot(Ages, aes(x = Var, y = Coef)) + theme_bw() + geom_boxplot() + facet_wrap(~ MDL) + 
-  labs(title ="Coefficient estimates by model", x = "Age Group", y = "Estimated Coefficient")
+ggplot(Ages, aes(x = Var, y = Coef)) + 
+  geom_point(aes(x= Var, y=Coef),alpha = 0.5) + 
+  geom_boxplot(aes(fill=Coef), alpha = 0.5) +
+  scale_fill_continuous(low="gold", high="red",guide = FALSE) +
+  geom_smooth(aes(x= REGX, y=Coef),col ="black",se = T,span = 0.8, alpha=0.5) +
+  facet_wrap(~MDL)  + 
+  labs(title ="Coefficient estimate ranges by model", x = "Age Group", y = "Estimated Coefficient") 
 
-ggplot(Ages, aes(x = Var, y = Coef)) + theme_bw() + geom_boxplot() + facet_wrap(~ YEAR) + 
-  labs(title ="Coefficient estimates by age", x = "Age Group", y = "Estimated Coefficient")
+ggplot(Ages, aes(x = Var, y = Coef)) + theme_bw() + 
+  geom_point(aes(x= Var, y=Coef),alpha = 0.5) + 
+  geom_boxplot(aes(fill=Coef), alpha = 0.5) +
+  geom_smooth(aes(x= REGX, y=Coef),col = "black", se = T,span = 0.8) +
+  facet_wrap(~YEAR)  + 
+  labs(title ="Coefficient estimate ranges by year", x = "Age Group", y = "Estimated Coefficient") 
 
 
 
-ggplot(Ages, aes(x = Var, y = Coef, ymin = lower, ymax = upper)) + theme_bw() + geom_errorbar() + 
+ggplot(Ages, aes(x = Var, y = Coef, ymin = lower, ymax = upper)) + theme_bw() + geom_errorbar() +
   facet_grid(YEAR~ MDL)   + labs(title ="Coefficient error bars by year and model", x = "Age Group", y = "Estimated Coefficient") 
 
 
-ggplot(Ages, aes(x = Var, y = Coef, ymin = lower, ymax = upper)) + theme_bw() + geom_pointrange() + 
+ggplot(Ages, aes(x = Var, y = Coef, ymin = lower, ymax = upper)) + theme_bw() + geom_pointrange() +
+  geom_point(aes(x= REGX, y=Coef, color=as.factor(sig0.05))) +
+  geom_smooth(aes(x= REGX, y=Coef, color=as.factor(YEAR)),se = T,span = 0.8) +
   facet_grid(YEAR~ MDL)  + labs(title ="Coefficient estimate ranges by year and model", x = "Age Group", y = "Estimated Coefficient") 
 
+ggplot(Ages, aes(x = Var, y = Coef, ymin = lower, ymax = upper)) + theme_bw() + geom_pointrange() +
+  geom_point(aes(x= REGX, y=Coef, color=as.factor(YEAR))) +
+  geom_smooth(aes(x= REGX, y=Coef, color=as.factor(YEAR)),se = T,span = 0.8) +
+  facet_wrap(~MDL)  + labs(title ="Coefficient estimate ranges by model", x = "Age Group", y = "Estimated Coefficient") 
+
+plot <- ggplot(Ages, aes(x = Var, y = Coef, ymin = lower, ymax = upper)) + theme_bw() + geom_pointrange() +
+  geom_point(aes(x= REGX, y=Coef, color=as.factor(YEAR))) +
+  geom_smooth(aes(x= REGX, y=Coef, color=as.factor(YEAR)),se = T,span = 0.8) +
+  facet_wrap(~YEAR)  + labs(title ="Coefficient estimate ranges by year", x = "Age Group", y = "Estimated Coefficient") 
 
 
 ##now lookin at only non-age variables:
@@ -55,30 +95,10 @@ ggplot(NAges, aes(x = Var, y = Coef)) + theme_bw() + geom_bar(stat = "identity")
 
 
 
-
-#########
-#########
-########
-# SMOOTHING *****
-###############
-
-Ages$Coef <- as.integer(Ages$Coef)
-
-
-# creating a variable for x axis to run smoothing regression
-
-Ages$REGX <- 0
-r <- length(unique(Ages$Var))
-for (i in 1:r) {
-  var <- unique(Ages$Var)[i]
-  num <- rank(unique(Ages$Var))[i]
-  Ages$REGX <- ifelse(Ages$Var == var, rank(unique(Ages$Var))[i], Ages$REGX)
-}
-
 ## loess smoothing
 ggplot(Ages, aes(x= REGX, y=Coef, color=as.factor(YEAR))) +
   geom_point() +
-  geom_smooth(se = FALSE,span = 0.8) +
+  geom_smooth(se = T,span = 0.8) +
   facet_wrap(~MDL) +
   labs(title ="Coefficient estimate trends by year and model -- loess smoothing", x = "Age Group", y = "Estimated Coefficient") 
 
@@ -95,4 +115,5 @@ ggplot(Ages, aes(x= REGX, y=Coef, color=as.factor(YEAR))) +
   geom_smooth(method = "lm", formula = y ~ splines::bs(x, 3), se = FALSE) +
   facet_wrap(~MDL) +
   labs(title ="Coefficient estimate trends by year and model -- linear spline degree 3", x = "Age Group", y = "Estimated Coefficient") 
+
 
