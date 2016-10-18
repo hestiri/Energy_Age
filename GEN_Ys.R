9be71bddcf3f13be05148094d213400dafd35354
hestiri <estiri.hossein@gmail.com>
2016-10-17 17:30:30 -0700

    psuedo-cohort analysis -- plot 4

--- GEN_Ys.R
+++ GEN_Ys.R
@@ -7,11 +7,13 @@ A_Age5sIN87 <- subset(OUTPUT, (
     (OUTPUT$Var == "age05to09" & OUTPUT$YEAR == 1990) |
     (OUTPUT$Var == "age20to24" & OUTPUT$YEAR == 2005) |
     (OUTPUT$Var == "age25to29" & OUTPUT$YEAR == 2009)
-  )
-  )
+)
+)
 
-A_Age5sIN87$cohort <- "a_5sIN87"
-                
+A_Age5sIN87$cohort <- "Cohort 1"
+A_Age5sIN87$cohortXL <- "Cohort 01: Age <5 in 1987"
+
+
 # unique(OUTPUT$Var)
 B_Age9sIN87 <- subset(OUTPUT, (
   (OUTPUT$Var == "age05to09" & OUTPUT$YEAR == 1987) | 
@@ -21,7 +23,8 @@ B_Age9sIN87 <- subset(OUTPUT, (
 )
 ) 
 
-B_Age9sIN87$cohort <- "b_9sIN87"
+B_Age9sIN87$cohort <- "Cohort 2"
+B_Age9sIN87$cohortXL <- "Cohort 02: Age 5-9 in 1987"
 
 
 C_Age14sIN87 <- subset(OUTPUT, (
@@ -32,7 +35,8 @@ C_Age14sIN87 <- subset(OUTPUT, (
 )
 ) 
 
-C_Age14sIN87$cohort <- "c_14sIN87"
+C_Age14sIN87$cohort <- "Cohort 3"
+C_Age14sIN87$cohortXL <- "Cohort 03: Age 10-14 in 1987"
 
 D_Age19sIN87 <- subset(OUTPUT, (
   (OUTPUT$Var == "age15to19" & OUTPUT$YEAR == 1987) | 
@@ -42,7 +46,8 @@ D_Age19sIN87 <- subset(OUTPUT, (
 )
 )
 
-D_Age19sIN87$cohort <- "d_19sIN87"
+D_Age19sIN87$cohort <- "Cohort 4"
+D_Age19sIN87$cohortXL <- "Cohort 04: Age 15-19 in 1987"
 
 
 E_Age24sIN87 <- subset(OUTPUT, (
@@ -53,7 +58,8 @@ E_Age24sIN87 <- subset(OUTPUT, (
 )
 )
 
-E_Age24sIN87$cohort <- "e_24sIN87"
+E_Age24sIN87$cohort <- "Cohort 5"
+E_Age24sIN87$cohortXL <- "Cohort 05: Age 20-24 in 1987"
 
 F_Age29sIN87 <- subset(OUTPUT, (
   (OUTPUT$Var == "age25to29" & OUTPUT$YEAR == 1987) | 
@@ -63,7 +69,8 @@ F_Age29sIN87 <- subset(OUTPUT, (
 )
 )
 
-F_Age29sIN87$cohort <- "f_29sIN87"
+F_Age29sIN87$cohort <- "Cohort 6"
+F_Age29sIN87$cohortXL <- "Cohort 06: Age 25-29 in 1987"
 
 
 G_Age34sIN87 <- subset(OUTPUT, (
@@ -74,7 +81,8 @@ G_Age34sIN87 <- subset(OUTPUT, (
 )
 )
 
-G_Age34sIN87$cohort <- "g_34sIN87"
+G_Age34sIN87$cohort <- "Cohort 7"
+G_Age34sIN87$cohortXL <- "Cohort 07: Age 30-34 in 1987"
 
 H_Age39sIN87 <- subset(OUTPUT, (
   (OUTPUT$Var == "age35to39" & OUTPUT$YEAR == 1987) | 
@@ -84,7 +92,9 @@ H_Age39sIN87 <- subset(OUTPUT, (
 )
 )
 
-H_Age39sIN87$cohort <- "h_39sIN87"
+H_Age39sIN87$cohort <- "Cohort 8"
+H_Age39sIN87$cohortXL <- "Cohort 08: Age 35-39 in 1987"
+
 
 I_Age44sIN87 <- subset(OUTPUT, (
   (OUTPUT$Var == "age40to44" & OUTPUT$YEAR == 1987) | 
@@ -94,7 +104,8 @@ I_Age44sIN87 <- subset(OUTPUT, (
 )
 )
 
-I_Age44sIN87$cohort <- "i_44sIN87"
+I_Age44sIN87$cohort <- "Cohort 9"
+I_Age44sIN87$cohortXL <- "Cohort 09: Age 40-44 in 1987"
 
 J_Age49sIN87 <- subset(OUTPUT, (
   (OUTPUT$Var == "age45to49" & OUTPUT$YEAR == 1987) | 
@@ -104,7 +115,9 @@ J_Age49sIN87 <- subset(OUTPUT, (
 )
 )
 
-J_Age49sIN87$cohort <- "j_49sIN87"
+J_Age49sIN87$cohort <- "Cohort 10"
+J_Age49sIN87$cohortXL <- "Cohort 10: Age 45-49 in 1987"
+
 
 K_Age54sIN87 <- subset(OUTPUT, (
   (OUTPUT$Var == "age50to54" & OUTPUT$YEAR == 1987) | 
@@ -114,7 +127,8 @@ K_Age54sIN87 <- subset(OUTPUT, (
 )
 )
 
-K_Age54sIN87$cohort <- "k_54sIN87"
+K_Age54sIN87$cohort <- "Cohort 11"
+K_Age54sIN87$cohortXL <- "Cohort 11: Age 50-54 in 1987"
 
 L_Age59sIN87 <- subset(OUTPUT, (
   (OUTPUT$Var == "age55to59" & OUTPUT$YEAR == 1987) | 
@@ -124,7 +138,9 @@ L_Age59sIN87 <- subset(OUTPUT, (
 )
 )
 
-L_Age59sIN87$cohort <- "l_59sIN87"
+L_Age59sIN87$cohort <- "Cohort 12"
+L_Age59sIN87$cohortXL <- "Cohort 12: Age 55-59 in 1987"
+
 
 GEN_Y <- rbind(A_Age5sIN87,B_Age9sIN87,C_Age14sIN87,D_Age19sIN87,E_Age24sIN87,F_Age29sIN87,
                G_Age34sIN87,H_Age39sIN87,I_Age44sIN87,J_Age49sIN87,K_Age54sIN87,L_Age59sIN87)
@@ -148,31 +164,33 @@ for (i in 1:r) {
 }
 
 
-plotG <- ggplot(GEN_Y, aes(x = Var, y = Coef, color = cohort, group = cohort)) +
+plotG <-
+  ggplot(GEN_Y, aes(x = Var, y = Coef, color = cohortXL, group = cohortXL)) +
   theme_bw() + 
   geom_point()+ #geom_line()+
   # geom_point(aes(color=as.factor(cohort))) +
-  facet_wrap(~ MDL, ncol = 4) +
+  facet_wrap(~ varnams, ncol = 2) +
   # geom_linerange(alpha=0.3) +
   geom_smooth(aes(x= REGX, y=Coef),se = F,span = 0.8) +
-  guides(col = guide_legend(nrow = 1,"Cohort",title.position = "top")) +
+  guides(col = guide_legend(nrow = 3,"Cohort",title.position = "top")) +
+  # guides(col = guide_legend(nrow = 1,"Cohort",title.position = "top")) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   theme(strip.text.x = element_text(size=9, face="bold"),
         strip.text.y = element_text(size=9, face="bold")) +
   theme(legend.position = "bottom") +
-  labs(title ="Coefficient estimate ranges by model for the 1987 cohorts", x = "Age Group", y = "Estimated Coefficient")  
+  labs(title ="Coefficient estimate ranges by model for 1987 cohorts", x = "Age Group", y = "Estimated Coefficient")  
 
-ggsave("graphics/plotG.png", plotG, dpi = 500, width = 12, height = 5)
+ggsave("graphics/plot4.pdf", plotG, dpi = 500, width = 9, height = 18)
 
-  # 
-  # ggplot(GEN_Y, aes(x = Var, y = Coef)) + #+ theme_bw() + geom_linerange(alpha=0.3) +
-  #   geom_smooth(aes(x= REGX, y=Coef, color=as.factor(MDL)),se = T,span = 0.8) +
-  #   geom_point(aes(x= REGX, y=Coef),alpha=0.4,shape=19) +
-  #   geom_smooth(aes(x= REGX, y=Coef, color=as.factor(MDL)),se = F,span = 0.8) +
-  #   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  #   theme(legend.position = "bottom") +
-  #   theme(strip.text.x = element_text(size=9, face="bold"),
-  #         strip.text.y = element_text(size=9, face="bold")) +
-  #   guides(col = guide_legend(nrow = 4,"Model",title.position = "top")) +
-  #   facet_wrap(~MDL, ncol=4)  + labs(title ="Coefficient estimate ranges by year", x = "Age Group", y = "Estimated Coefficient")  
-  # 
+# 
+# ggplot(GEN_Y, aes(x = Var, y = Coef)) + #+ theme_bw() + geom_linerange(alpha=0.3) +
+#   geom_smooth(aes(x= REGX, y=Coef, color=as.factor(MDL)),se = T,span = 0.8) +
+#   geom_point(aes(x= REGX, y=Coef),alpha=0.4,shape=19) +
+#   geom_smooth(aes(x= REGX, y=Coef, color=as.factor(MDL)),se = F,span = 0.8) +
+#   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+#   theme(legend.position = "bottom") +
+#   theme(strip.text.x = element_text(size=9, face="bold"),
+#         strip.text.y = element_text(size=9, face="bold")) +
+#   guides(col = guide_legend(nrow = 4,"Model",title.position = "top")) +
+#   facet_wrap(~MDL, ncol=4)  + labs(title ="Coefficient estimate ranges by year", x = "Age Group", y = "Estimated Coefficient")  
+# 
